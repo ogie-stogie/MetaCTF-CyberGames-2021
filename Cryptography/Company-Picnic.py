9544e8c26b3fe08f4bd7e652d3eb9184d20164f3
ogie-stogie <taylorbart27@gmail.com>
2021-12-05 13:43:16 -0700

    CTF Ended

--- Cryptography/Company-Picnic.py
+++ Cryptography/Company-Picnic.py
@@ -1,2 +1,20 @@
+import os
+import sys
+
 if __name__ == "__main__":
-    pass
+    with open("Key_List/public_keys.txt", "r") as f:
+        lines = f.readlines()
+    time_to_run = False
+    for line in lines:
+        if line[0] == "N":
+            n = line.split()[-1]
+            print(line)
+            time_to_run = False
+        elif line[0] == "e":
+            e = line.split()[-1]
+            print(line)
+            time_to_run = True
+        if time_to_run:
+            cmd = f"../venv/bin/rsactftool -n {n} -e {e} --private"
+            os.system(cmd)
+            break
--- Cryptography/Size-Matters.py
+++ Cryptography/Size-Matters.py
@@ -1,19 +1,11 @@
+import subprocess
+import sys
+import rsactftool
 # N = pq
 # (p-1)(q-1) mod e = 1
 if __name__ == "__main__":
     e = 257
     n = 0x592f144c0aeac50bdf57cf6a6a6e135
     ciphertext = 0x2526512a4abf23fca755defc497b9ab
-    for p in range(1,n,2):
-        q = n/p
-        if (q-1)*(q-1) % e != 0:
-            d = 0
-            if (e*d) % n == 1:
-
-            plaintext = (ciphertext**d) % n
-            print(plaintext)
-            # bytes_object = str(int(decrypted_message, 16))
-            # # ascii_string = bytes_object.decode("hex")
-            # print(ascii_string)
-            # if "MetaCTF{" in decrypted_message:
-            #     print(decrypted_message)
+    result = subprocess.run(["venv/bin/rsactftool", "-n", str(n), "-e", str(e), "--uncipher", str(ciphertext)])
+    print(result)
